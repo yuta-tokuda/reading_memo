192ab086c661ae06242fcd553d907c6ed25c9329
tokuda yuta <yuta.tokuda1123112309@gmail.com>
2019-10-18 09:57:28 +0900

    book周りsetup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+
+  resources :books do
+    collection do
+      match :index, via: %i[get post]
+      match 'create_new', to: 'books#create', via: 'post'
+    end
+  end
 end
